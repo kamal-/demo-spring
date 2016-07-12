dab4397cb1d7a7ff809977d8f386f59e76c825ce
kamal <csanwalkamal@gmail.com>
2016-07-12 16:21:44 +0530

    Test

---
+++ src/com/j/resthandler/RequestController.java
@@ -0,0 +1,41 @@
+package com.j.resthandler;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+
+	@RequestMapping(value="/login",method=RequestMethod.GET,produces="application/json")
+	@ResponseBody
+	public List<String> loginFunction()
+	{
+		List<String> s=new ArrayList<>();
+		s.add("A");s.add("A");s.add("A");s.add("A");s.add("A");s.add("A");
+		return s;
+	}
+	@RequestMapping(value="*",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String homeFunction()
+	{
+		List<String> s=new ArrayList<>();
+		s.add("A");s.add("A");s.add("A");s.add("A");s.add("A");s.add("A");
+		return "Welcome user";
+	}
+	@RequestMapping(value="*/*",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String redirectFunction()
+	{
+		List<String> s=new ArrayList<>();
+		s.add("A");s.add("A");s.add("A");s.add("A");s.add("A");s.add("A");
+		return "Welcome user";
+	}
+}
